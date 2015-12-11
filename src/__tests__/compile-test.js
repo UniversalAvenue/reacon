0a1153fe99f58e83fde343a5fc8845177a3af18f
Daniel Werthén <danielwerthen@gmail.com>
2015-12-11 11:38:36 +0100

    Test scripts

---
+++ src/__tests__/compile-test.js
@@ -0,0 +1,24 @@
+jest.autoMockOff();
+
+const compile = require('../index').compile;
+
+describe('Compile', () => {
+  it('should produce a simple p tag', () => {
+    const res = compile({
+      component: 'p',
+      children: 'TEMPLATE Fred lives in ${data.city}',
+    });
+    expect(res({ city: 'Stockholm' }).children).toEqual('Fred lives in Stockholm');
+  });
+  it('should produce a complex div tag', () => {
+    const res = compile({
+      component: 'div',
+      children: [
+        { key: 'p1', component: 'p', children: 'Lovely' },
+        { key: 'p2', component: 'p', children: 'Lovely f2' },
+        { key: 'p3', component: 'p', children: 'TEMPLATE Lovely ${data.str}' },
+      ],
+    });
+    expect(res({ str: 'weather2' }).children[2].children).toEqual('Lovely weather');
+  });
+});
