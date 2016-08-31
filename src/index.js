6bd3c443b7b5e0db63e20dda98c600a5a15fdc6a
Daniel Werthén <danielwerthen@gmail.com>
2016-08-31 17:11:42 +0200

    Stuff

--- src/index.js
+++ src/index.js
@@ -1,123 +1,134 @@
 import React from 'react';
 import _ from 'lodash';
 
-const reducer = fn => (sum, str, key) => {
-  sum[key] = fn(str, key);
-  return sum;
-};
+function identity(id) {
+  return id;
+}
 
-
-const template = (str) => {
-  const compiled = _.template(str);
-  return (data = {}, props = {}) => {
-    return compiled({ data, props });
-  };
-};
-
-const getter = (str) => {
-  return (data = {}, props = {}) => {
-    return _.get({ data, props }, str);
-  };
-};
-
-const templatePattern = /^TEMPLATE (.+)$/;
-const getterPattern = /^GET (.+)$/;
-
-const parsers = [
-  { pattern: templatePattern, fn: template },
-  { pattern: getterPattern, fn: getter },
-];
-
-const parse = (str = '') => {
-  let result = () => str;
-  _.find(parsers, ({ pattern, fn }) => {
-    if (!pattern) {
-      return true;
-    }
-    const match = pattern.exec(str);
-    if (match && match[1]) {
-      result = fn(match[1]);
-      return true;
-    }
-    return false;
-  });
-  return result;
-};
-
-export const interpolate = (args = {}) => {
-  if (_.keys(args).length < 1) {
-    return args;
+export function sliceModifiers(content) {
+  if (_.isArray(content)) {
+    const lastIdx = content.length - 1;
+    return {
+      mods: content.slice(0, lastIdx),
+      target: content[lastIdx],
+    };
   }
-  return _.reduce(args, reducer(str => {
-    if (_.isArray(str)) {
-      return str.map(s => interpolate(s));
-    }
-    if (_.isObject(str)) {
-      return interpolate(str);
-    }
-    if (_.isString(str)) {
-      return parse(str);
+  return {
+    mods: [],
+    target: content,
+  };
+}
+
+function deepTap(obj, tap, isTapable) {
+  if (isTabable && isTabable(obj)) {
+    return tap(obj);
+  } else if (_.isArray(obj)) {
+    return obj.map(item => deepTap(item, tap));
+  } else if (_.isObject(obj)) {
+    return Object.keys(obj)
+      .reduce((sum, key) => Object.assign(sum, {
+        [key]: deepTap(obj[key], tap),
+      }), {});
+  }
+  return tap(obj);
+}
+
+function tokenize(str) {
+  return new RegExp(`^${str.toUpperCase()}\\s*(.+)$`);
+}
+
+export function injectParams(injectors) {
+  const injs = Object.keys(injectors)
+    .map(key => [tokenize(key), injectors[key]]);
+
+  function applyInjector(str, params) {
+    let match;
+    const found = injs.find(i => {
+      match = i[0].exec(str);
+      return match;
+    });
+    if (match) {
+      return found[1](match[1], params);
     }
     return str;
-  }), {});
-};
+  }
 
-export const inject = (compiled) => (data = {}, props = {}) => {
-  if (_.isFunction(compiled)) {
-    return compiled(data, props);
-  }
-  if (_.isArray(compiled)) {
-    return compiled.map(c => c(data, props));
-  }
-  if (!_.isObject(compiled) || _.keys(compiled).length < 1) {
-    return compiled;
-  }
-  return _.reduce(compiled, reducer(fn => {
-    if (_.isFunction(fn)) {
-      return fn(data, props);
+  return (content, params) => {
+    function inject(val) {
+      if (!_.isString(val)) {
+        return val;
+      }
+      return applyInjector(val, params);
     }
-    if (_.isArray(fn)) {
-      return _.map(fn, fnn => inject(fnn)(data, props));
-    }
-    return inject(fn)(data, props);
-  }), {});
-};
-
-export const compile = (node, map = id => id) => {
-  if (_.isString(node)) {
-    const fn = inject(parse(node));
-    return fn;
-  }
-  if (_.keys(node) < 1) {
-    return () => node;
-  }
-  const propsFn = interpolate(_.omit(node, 'children', 'component'));
-  const children = node.children;
-  if (_.isString(children)) {
-    propsFn.children = parse(children);
-  } else if (_.isArray(children)) {
-    const tmp = children.map(child => compile(child, map));
-    propsFn.children = (data, props) => tmp.map(fn => fn(data, props));
-  } else if (_.isObject(children)) {
-    propsFn.children = compile(children, map);
-  } else if (children) {
-    propsFn.children = children;
-  }
-  return (data, staticProps) => {
-    const props = inject(propsFn)(data, staticProps);
-    return map({
-      component: node.component,
-      ...staticProps,
-      ...props,
-    });
+    return deepTap(content, inject);
   };
-};
+}
 
-export const reactify = (node, components = {}) => {
-  const result = compile(node, ({ component, ...props }) => {
-    const Component = components[component] || component;
-    return <Component {...props}/>;
-  });
+export function inflater({
+  modifiers = {},
+  injectors = {},
+  PromiseRef = Promise,
+}) {
+  const inject = injectParams(injectors);
+  function applyModifier(sum, mod) {
+    return sum.then(outerParams => {
+      const {
+        type,
+        params = {},
+        props,
+      } = inject(mod, outerParams);
+      let modification = identity;
+      if (type) {
+        if (!modifiers[type]) {
+          return PromiseRef.reject(`Could not find a matching modifier named ${type}`);
+        }
+        modification = modifiers[type];
+      }
+      return modification(props, {
+        ...outerParams,
+        ...params,
+      });
+    });
+  }
+  return (content, params = {}) => {
+    const {
+      mods,
+      target,
+    } = sliceModifiers(content);
+    if (!target) {
+      return target;
+    }
+    return mods.reduce(applyModifier, PromiseRef.resolve(params))
+      .then(output => inject(target, output));
+  };
+}
 
-  return data => props => result(data, props);
-};
+function isReacon(obj) {
+  return _.isObject(obj) && _.isString(obj.type);
+}
+
+export function reactify(components) {
+  function doReactify(content) {
+    return deepTap(obj, render, isReacon);
+  }
+
+  function render(obj) {
+    const {
+      type,
+      props = {},
+    } = obj;
+    if (!type) {
+      throw new Error('Tried to reactify an object without type');
+    }
+    let Component = components[type];
+    if (!Component) {
+      if (type[0] === type[0].toUpperCase()) {
+        throw new Error(`Couldn\'t find a matching component for ${type}`);
+      }
+      Component = type;
+    }
+    const reactifiedProps = doReactify(props);
+    return <Component {...reactifiedProps} />;
+  }
+  return doReactify;
+}
