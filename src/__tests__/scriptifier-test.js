b9e1527e23a4014e8dff3ed1e726ba10aa1cb4b0
Daniel Werthén <danielwerthen@gmail.com>
2016-09-02 16:44:57 +0200

    Better now

--- src/__tests__/scriptifier-test.js
+++ src/__tests__/scriptifier-test.js
@@ -1,9 +1,10 @@
-import { scriptifier } from '../index';
+import { scriptifier, reactifier } from '../index';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 
 describe('Scriptifier', () => {
   const scriptify = scriptifier({});
+  const reactify = reactifier({});
   it('should produce something', () => {
     const Component = scriptify({
       type: 'div',
@@ -18,5 +19,22 @@ describe('Scriptifier', () => {
     });
     const res = ReactDOM.renderToStaticMarkup(<Component />);
     expect(res).toEqual('<div><p>Hola</p></div>');
+  });
+  it('should be equal to reactify approach', () => {
+    const content = {
+      type: 'div',
+      props: {
+        children: {
+          type: 'p',
+          props: {
+            children: 'Hola',
+          },
+        },
+      },
+    };
+    const Component = scriptify(content);
+    const a = ReactDOM.renderToStaticMarkup(<Component />);
+    const b = ReactDOM.renderToStaticMarkup(reactify(content));
+    expect(a).toEqual(b);
   });
 });
